fa11dde38788d50e17fe8ff2ea0a8f5c64d7d224
chiangchiang <love@AEWIN1138.aewin.com>
2024-01-02 16:54:38 +0800

    .

--- tests/basic.spec.js
+++ tests/basic.spec.js
@@ -1,22 +1,26 @@
 // @ts-check
 const { test, expect } = require('@playwright/test');
+let context;
+let page;
+//import{ LoginPage } from '../pages/login-page.js';
 let flagChangePassword = 0;
 
 test.use({
 	ignoreHTTPSErrors: true,
   });
 // this is very imortant didnt discord it 
+// [ref](https://stackoverflow.com/questions/67048422/ignore-ssl-errors-with-playwright-code-generation)
+// [ref-origin](https://github.com/microsoft/playwright/issues/2814)
 
 test.skip(' just test in the amazing thing to test number connceting ', async({ page}) => {
 	//await page.goto('https://185.199.111.153');
 	await page.goto('http://192.168.120.241:3000/');
 });
-test('try password', async ({page}) => {
-	
-});
-
-test.only('change password ', async ({ page }) => {
-
+//test('goto google', async ({page}) => {
+//	await page.goto('https://google.com/');
+//	//
+//});
+test('change password ', async ({page }) => {
 	await page.goto('https://192.168.120.218/');
 	await page.goto('https://192.168.120.218/#dashboard');
 	await page.goto('https://192.168.120.218/#login');
@@ -28,7 +32,20 @@ test.only('change password ', async ({ page }) => {
 	//await page.getByPlaceholder('Password').fill('11111111');
 	//await page.getByRole('button', { name: '登入' }).click();
 	await page.getByRole('button', { name: 'Sign me in' }).click();
-
+	await page.waitForTimeout(3000);
+	const look = await page.getByText('Password should be changed').isVisible();
+	//const look1 = await page.getByText('Password should be changed').toBeVisible();
+	//OMG  the official documentation is written poorly ==
+	//const look2 = await expect(page.getByText('Password should be changed')).toBeVisible();
+	console.log(look);
+	//console.log(look2);
+	//const text1 = await Response.statusText();
+	//console.log(text1);
+	//const text2 = await apiResponse.text();
+	//console.log(text2);
+	
+	if(await page.getByText('Password should be changed').isVisible()){
+		console.log(' change password ');
 	await page.getByPlaceholder('New Password').fill('11111111');
 	await page.getByPlaceholder('Confirm Password').fill('11111111');
 	page.once('dialog', dialog => {
@@ -36,19 +53,79 @@ test.only('change password ', async ({ page }) => {
 		dialog.dismiss().catch(() => {});
 	});
 	await page.getByRole('button', { name: 'Submit' }).click();
-	//await page.getByPlaceholder('Username').fill('');
-	//await page.getByRole('textbox', { name: 'Taiwan - 中文 (正體)' }).click();
-	//await page.getByRole('option', { name: 'US - English' }).click();
 	await page.getByPlaceholder('Username').fill('admin');
-	//await page.getByPlaceholder('Username').press('Tab');
+	////await page.getByPlaceholder('Username').press('Tab');
 	await page.waitForTimeout(1000);
 	await page.getByPlaceholder('Password', { exact: true }).fill('11111111');
-	await page.waitForTimeout(4000);
-	
+	await page.waitForTimeout(2000);
+	}
+	else
+	{
+	await page.getByPlaceholder('Username').fill('admin');
+	await page.getByPlaceholder('Password', { exact: true }).fill('11111111');
+	await page.getByRole('button', { name: 'Sign me in' }).click();
+	}
 
+	//await context.tracing.stop({path: 'test4_trace.zip'});
 	
 });
-
+/*
+ * {{{ 
+ * 
+function loveLogin({page}){
+	await page.goto('https://192.168.120.218/#login');
+	await page.getByPlaceholder('Username').fill('admin');
+	await page.getByPlaceholder('Password', { exact: true }).fill('11111111');
+	await page.getByRole('button', { name: 'Sign me in' }).click();
+};
+ * }}}
+*/
+/*
+test.beforeAll(async({browser}) =>{
+	context = await browser.newContext();
+	await context.tracing.start({screenshorts:true, snapshots:true});
+	page = (await context).newPage();
+});
+*/
+test('login ', async({page}) => {
+//	context = await browser.newContext();
+	//await context.tracing.start({screenshorts: true , snapshots:true });
+	await page.waitForTimeout(10000);
+	await page.goto('https://192.168.120.218/#login');
+	await page.getByPlaceholder('Username').fill('admin');
+	await page.getByPlaceholder('Password', { exact: true }).fill('11111111');
+	await page.getByRole('button', { name: 'Sign me in' }).click();
+	//await context.tracing.stop({path: 'test3_trace.zip'});
+});
+test('sensor ' , async({page}) => {
+	//loveLogin();
+	/*
+	await page.goto('https://192.168.120.218/#login');
+	await page.getByPlaceholder('Username').fill('admin');
+	await page.getByPlaceholder('Password', { exact: true }).fill('11111111');
+	await page.getByRole('button', { name: 'Sign me in' }).click();
+	const look = await page.getByText('Password should be changed').isVisible();
+	await page.goto('https://192.168.120.218/#sensors');
+	await page.waitForTimeout(2000);
+	await page.screenshot({path: 'screenshot/sensor.png',fullPage: true });
+	if(await page.getByText('Password should be changed').isVisible()){
+		console.log(' change password ');
+	await page.getByPlaceholder('New Password').fill('11111111');
+	await page.getByPlaceholder('Confirm Password').fill('11111111');
+	page.once('dialog', dialog => {
+		console.log(`Dialog message: ${dialog.message()}`);
+		dialog.dismiss().catch(() => {});
+	});
+	await page.getByRole('button', { name: 'Submit' }).click();
+	//await page.getByPlaceholder('Username').fill('admin');
+	////await page.getByPlaceholder('Username').press('Tab');
+	//await page.waitForTimeout(1000);
+	//await page.getByPlaceholder('Password', { exact: true }).fill('11111111');
+	//await page.waitForTimeout(4000);
+	}
+	*/
+	await page.goto('https://192.168.120.218/#sensors');
+});
 
 test.skip('login eng', async ({ page }) => {
 	await page.goto('about:blank');
@@ -62,6 +139,8 @@ test.skip('login eng', async ({ page }) => {
 	await page.getByPlaceholder('Username').press('Tab');
 	await page.getByPlaceholder('Password', { exact: true }).fill('admin');
 	await page.getByRole('button', { name: 'Sign me in' }).click();
+	//await page.getByText('Password should be changed').click();
+	await expect(page.getByText('Password should be changed')).toBeVisible();	
 	await page.getByPlaceholder('New Password').click();
 	await page.getByPlaceholder('New Password').fill('11111111');
 	await page.getByPlaceholder('New Password').press('Tab');
@@ -81,4 +160,5 @@ test.skip('login eng', async ({ page }) => {
 	await page.getByPlaceholder('Password', { exact: true }).fill('11111111');
 	await page.getByRole('button', { name: 'Sign me in' }).click();
 });
+
 
