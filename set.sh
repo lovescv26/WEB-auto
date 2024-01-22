880c6ecd389872c03e9dfdc095c6f81cbe858a14
chiangchiang <love@AEWIN1138.aewin.com>
2024-01-22 15:29:54 +0800

    fix bash number => EXPR

--- set.sh
+++ set.sh
@@ -2,9 +2,9 @@
 ### {{{ 
 ### ----------------------
 ### created : Mon Jan 22 11:14:41 CST 2024
-### Version : 0 
+### Version : 1 
 ### author : lovelovequeen
-### time   : Mon Jan 22 11:19:22 CST 2024
+### time   : Mon Jan 22 11:31:11 CST 2024
 ### [ref](https://linuxhandbook.com/bash-variables/)
 ### ----
 ### readme : this is need to let user input ip 
@@ -12,19 +12,34 @@
 ### }}}
 ip="";
 validate_ip="";
-ip_flag=0;
-while [ ${ip_flag} == 0 ]
+ip_flag=1;
+while [ ${ip_flag} == 1 ]
 do
-echo " U need to input IP address ";
+echo -e " \nU need to input IP address ";
 read ip;
-echo " double check Ur IP address ";
+echo -e " if U ip address is  :: [${ip}]  ";
+echo -e " press \"Y\" \"y\"  or \"0\" is comfirm ip address  ";
 read validate_ip;
-if [[ ${ip} == ${validate_ip} ]]
-then
-	ip_flag=1;
-	echo " U input IP address is ${ip} ";
-else
-	echo " U ip address != validate ip address ";
-	echo -e "\n\n";
-fi
+case "${validate_ip}" in
+	Y) 
+		echo " U input IP address is ${ip} ";
+		ip_flag=0;;
+	y)
+		echo " U input IP address is ${ip} ";
+		ip_flag=0;;
+	0)
+		echo " U input IP address is ${ip} ";
+		ip_flag=0;;
+	*)
+		echo "plz redo " ;;
+esac
+done
+count_uploadfile=0;
+for file in $(ls ./tests/uploadFiles)
+do
+	#Extension=${file##*.}
+	#count_uploadfile=count_uploadfile+1;
+	#count_uploadfile=${count_uploadfile}++;
+	count_uploadfile=$((${count_uploadfile}+1));
+	echo  "[${count_uploadfile}->${file}]";
 done
