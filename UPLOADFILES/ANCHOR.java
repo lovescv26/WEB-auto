679ff2dc6054474713ed2333679c04468c8d1f1b
loveloveempress <love@AEWIN1138.aewin.com>
2024-02-06 15:05:20 +0800

    change : structure ->  change the file

---
+++ UPLOADFILES/ANCHOR.java
@@ -0,0 +1,9 @@
+public class Anchor{
+	public static void main(String[] args){
+		System.out.println(" this is anchor ");
+				/*
+dGhpcyBpcyBhbmNob3IKIE9SIOWPr+S7peiqquaYr+iomOmMhOm7niDpgoTkuI3mmK/ngrrkuoYg
+Z2l0ID09Cuawo+atuwphbnl3YXkgOnJlY29yZAoyMDI0LzAyLzA2Cg==
+*/
+	}
+}
